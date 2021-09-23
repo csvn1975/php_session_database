4ceb983085296c0dd1a87f556f4d04cb528dcd34
Nguyen Le <csvn1975khonnan@gmail.com>
2021-09-23 15:54:49 +0200

    add project session save in database

---
+++ config/database.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST' , 'localhost:3307');
+define('DB_USER' , 'root');
+define('DB_PASS' , 'root');
+define('DB_NAME' , 'db_session');
+define('DB_POST' , 'sessions');
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors","On");
+
+require_once './config/database.php';
+require_once './core/classes/DBSessionHandler.php';
+
+
+spl_autoload_register(function($className) {
+    echo $className;
+});
+
+$pdo = new PDO('mysql:host=' . DB_HOST . '; dbname=' . DB_NAME . ';charset=utf8mb4', DB_USER, DB_PASS);
+
+$sessionHandler = new Core\Classes\DBSessionHandler($pdo);
+session_set_save_handler($sessionHandler, false);
+
+session_start();
+
+$currentUserId = 33333;
+$_SESSION['currentUserId'] = $currentUserId;
+$_SESSION['cart'] = [
+    [
+        'user_id' => $currentUserId,  
+        'product_id' => random_int(1, 100000),  
+    ]
+];
+
+
+# if call this => add new session, otherwise update session
+#session_regenerate_id();
+#session_gc(); # expired automatic delete
+# session_destroy(); # => $sessionHandler->destroy
+
+$pdo = null;
